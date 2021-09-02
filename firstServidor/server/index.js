ef26725815d803d4fc0f5b13fc75145fdbbe7ed5
Mateus T <mateustavarestv@hotmail.com>
2021-09-01 22:55:20 -0300

    entendendo um pouco mais sobre servidor

--- firstServidor/server/index.js
+++ firstServidor/server/index.js
@@ -1,8 +1,15 @@
 const http = require('http')   //importando o modulo http
+const fs = require('fs')
+
 
 const server = http.createServer(function(request, response) {     //resquest e o que esta vindo na requisicao
-  console.log(request)                                        // response a resposta que vou dar para essa requisicao
-  response.end('Hello World!')
+  if(request.url === '/'){                                        // response a resposta que vou dar para essa requisicao
+    fs.readFile('../client/index.html', function(error, content){
+      response.end(content)
+    })}
+  if(request.url === '/teste'){
+    response.end('ok deuc certo')
+  }
 })
 
 server.listen(8080)

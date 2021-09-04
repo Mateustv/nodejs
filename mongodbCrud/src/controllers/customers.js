6af99fc47040c5a88936e300c72e888536d99397
Mateus T <mateustavarestv@hotmail.com>
2021-09-04 16:11:44 -0300

    Criando rotas para editar os usauarios cadastrado

--- mongodbCrud/src/controllers/customers.js
+++ mongodbCrud/src/controllers/customers.js
@@ -37,7 +37,45 @@ async function listUsers (req, res){
   })
 }
 
+async function indexEdit (req, res){
+  
+  const { id } = req.query
+  const user = await CustomersModel.findById(id)
+  
+  res.render('edit',{
+    title: 'Editar',
+    titleh1: 'Editar Usuarios',
+    user,
+  })
+}
+
+async function edit (req, res){
+  const { 
+    name,
+    age,
+    email,
+  } = req.body
+  
+  const {id}= req.params 
+  const user = await CustomersModel.findById(id)
+
+  user.name = name
+  user.age = age
+  user.email = email
+  user.save()
+
+  res.render('edit',{
+    title: 'Editar',
+    titleh1: 'Editar Usuarios',
+    user,
+    message: 'Usuario editado com sucesso' 
+  })
+}
+
+
 module.exports = {
   add,
   listUsers,
+  indexEdit,
+  edit,
 }
--- mongodbCrud/src/routes/index.js
+++ mongodbCrud/src/routes/index.js
@@ -2,12 +2,17 @@ const router = require('express').Router()
 
 const CustomersController = require('../controllers/customers')
 
+//Minhas rodas dentro do site
+
+//home
 router.get('/', (req, res) => {
   res.render('main',{ 
     title: 'Fazer um teste',
     titleh1: 'HOME'
   })
 })
+
+//Colcoar contatos
 router.get('/register', (req, res) => {
   res.render('register',{ 
     title: 'Form',
@@ -16,7 +21,11 @@ router.get('/register', (req, res) => {
 })
 router.post('/register/add',CustomersController.add)
 
+// listar contato
 router.get('/list', CustomersController.listUsers)
 
+//editar contatos
+router.get('/edit', CustomersController.indexEdit)
+router.post('/edit/:id',CustomersController.edit)
 
 module.exports = router
